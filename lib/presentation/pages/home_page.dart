d6b0b432cf13199c7f4f92d5cc0251e3e20d6ff0
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-25 03:06:51 +0500

    Add permissions for gallery access

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -3,14 +3,30 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_to_text_convertor/presentation/bloc/text_recognition_bloc.dart';
 import 'package:image_to_text_convertor/presentation/widgets/image_view.dart';
 import 'package:image_to_text_convertor/presentation/widgets/text_result.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+
+  Future<void> _requestPermission(BuildContext context) async {
+    PermissionStatus status = await Permission.photos.request();
+
+    if (status.isGranted) {
+
+      context.read<TextRecognitionBloc>().add(PickImageEvent());
+    } else {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Permission denied to access photos')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Text Recognizer')),
+      appBar:  AppBar(title: Text('Text Recognizer')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -19,14 +35,15 @@ class HomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
-                  onPressed: () =>
-                      context.read<TextRecognitionBloc>().add(PickImageEvent()),
-                  child: const Text('Pick Image')),
+                  onPressed: () => _requestPermission(context), // Request permission before picking image
+                  child: const Text('Pick Image'),
+                ),
                 ElevatedButton(
                   onPressed: () => context
                       .read<TextRecognitionBloc>()
                       .add(RecognizeTextEvent()),
-                  child: const Text('Recognize Text')),
+                  child: const Text('Recognize Text'),
+                ),
               ],
             ),
             const SizedBox(height: 20),
